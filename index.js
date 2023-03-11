60095fdb88da63c2c743bea4623de789bde96c05
Victor Ngigi Njoroge <vnjorogen2001@gmail.com>
2023-03-11 18:03:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+const employee = {
+  name : 'Sam'
+}
+console.log (employee)
+employee.name = 'Sam';
+
+function updateEmployeeWithKeyAndValue() {
+  const employee = {name: 'Sam'};
+  employee.streetAddress = '11 Broadway';
+    return employee;
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  
